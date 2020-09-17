e7f0c68d1900f52c7d6d6146ad4a8c5c78ec75e7
Ken <kenvu@protonmail.com>
2020-09-17 10:55:31 +0100

    Removed all comments in main.dart

--- lib/hn_theme.dart
+++ lib/hn_theme.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 
-final ThemeData defaultTheme = buildDefaultTheme();
+final ThemeData hnTheme = buildDefaultTheme();
 
 ThemeData buildDefaultTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
     textTheme: buildDefaultTextTheme(base.textTheme),
     primaryColor: Color(0x7FB069), // Bud Green
     accentColor: Color(0x2B2D42), // Space Cadet
     scaffoldBackgroundColor: Color(0xFAFFFD), // Baby Powder
     cardTheme: CardTheme(
-      color: Color(0xEBE9E9), // Platinum
-      shadowColor: Color(0xE8F1E4) // Lighter Bud Green
-    ),
-    buttonColor: Color(0x00A7E1) // Cerulean Crayola
+        color: Color(0xEBE9E9), // Platinum
+        shadowColor: Color(0xE8F1E4) // Lighter Bud Green
+        ),
+    buttonColor: Color(0x00A7E1), // Cerulean Crayola
   );
 }
 
@@ -41,11 +42,7 @@ TextTheme buildDefaultTextTheme(TextTheme base) {
       fontSize: 16,
       fontWeight: FontWeight.bold,
     ),
-    bodyText1: TextStyle(
-      fontSize: 14
-    ),
-    bodyText2: TextStyle(
-      fontSize: 12
-    ),
+    bodyText1: TextStyle(fontSize: 14),
+    bodyText2: TextStyle(fontSize: 12),
   );
 }
