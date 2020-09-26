0937e4a039e67dfdb7c64e5d1e92d06e9437d6fa
Ken <kenvu@protonmail.com>
2020-09-26 15:47:55 +0100

    added some extra docuemntation

--- test/features/hacker_news/data/models/hn_item_model_test.dart
+++ test/features/hacker_news/data/models/hn_item_model_test.dart
@@ -1,14 +1,32 @@
+import 'dart:convert';
+
 import 'package:HackerNews/features/hacker_news/data/models/hn_item_model.dart';
 import 'package:HackerNews/features/hacker_news/domain/entities/hn_item.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+// Note: For Unit tests, you never want to reach out to the outer world, and keep all the tests ran locally.
 
 void main() {
   final tHNModel = HackerNewsModel(id: 0);
 
   test('should be a subclass of hn_item entity', () async {
     // assert
-    expect (tHNModel, isA<HackerNewsItem>());
+    expect(tHNModel, isA<HackerNewsItem>());
 
     // act
+  });
+
+  group('fromJson', () {
+    test('should return a valid model', () async {
+      // arrange
+      final Map<String, dynamic> jsonMap = jsonDecode(fixture('live_data'));
+
+      //act
+
+      // assert
+
+    });
   });
 }
