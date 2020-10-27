77c86496190bc2f06e016786154643930cbb2921
Ken <kenvu@protonmail.com>
2020-10-27 12:17:36 +0000

    Initial commit of number_trivia feature

---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,5 @@
+import 'package:HackerNews/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
