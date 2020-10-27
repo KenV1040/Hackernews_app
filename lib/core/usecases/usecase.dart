f4a145d1d87c49bdfb811df5af41e7cf3f2feeba
Ken <kenvu@protonmail.com>
2020-10-27 12:40:27 +0000

    Modified usecase to have a NoParams class

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../error/failures.dart';
 
@@ -7,4 +8,9 @@ abstract class UseCase<Type, Params> {
   // Turn this into a function and print the params if you want to log the
   // call parameter
   Future<Either<Failure, Type>> call();
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
 }
